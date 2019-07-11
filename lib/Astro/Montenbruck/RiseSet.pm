85f0dd3d70a038f0af4f466345004f3254ff1141
Sergey Krushinsky <s.krushinsky@gateline.ru>
2019-07-11 23:51:06 +0300

    documentation

--- lib/Astro/Montenbruck/RiseSet.pm
+++ lib/Astro/Montenbruck/RiseSet.pm
@@ -181,6 +181,17 @@ sub _rst_function {
       }
 }
 
+# Return the standard altitude of the Moon.
+#
+# Arguments:
+#   - $r : Distance between the centers of the Earth and Moon, in km.
+# Returns:
+#   - Standard altitude in radians.
+sub _moon_rs_alt {
+    my ($y, $m, $d) = @_;
+    $H0_MOO
+}
+
 sub rst_event {
     my %arg = @_;
     my $pla = delete $arg{planet};
@@ -189,7 +200,7 @@ sub rst_event {
         h       => do {
             given( $pla ) {
                 $H0_SUN when $SU;
-                $H0_MOO when $MO;
+                _moon_rs_alt($arg{year}, $arg{month}, $arg{day}) when $MO;
                 default { $H0_PLA }
             }
         },
@@ -212,3 +223,164 @@ sub twilight {
         %arg
     );
 }
+
+
+1;
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Astro::Montenbruck::RiseSet — rise, set, transit.
+
+=head1 SYNOPSIS
+
+use Astro::Montenbruck::Ephemeris::Planet qw/:ids/;
+use Astro::Montenbruck::MathUtils qw/frac/;
+use Astro::Montenbruck::RiseSet', qw/:all/;
+
+# create function for calculating Moon rise for Munich, Germany,
+# on March 23, 1989.
+my $func = rst_event(
+    planet => $MO,
+    year   => 1989,
+    month  => 3,
+    day    => 23,
+    phi    => 48.1,
+    lambda => -11.6
+);
+
+# calculate rise
+# alternatively, use $EVT_SET for set, $EVT_TRANSIT for transit
+# as the first argument
+$func->(
+    $EVT_RISE,
+    on_event => sub {
+        my $jd = shift; # Standard Julian date
+        my $ut = frac(jd - 0.5) * 24; # UTC, 18.95 = 18h57m
+    }
+);
+
+
+=head1 VERSION
+
+Version 0.01
+
+
+=head1 DESCRIPTION
+
+Rise, set and transit times of celestial bodies. The calculations are based on
+I<"Astronomical Algorythms" by Jean Meeus>. The same subject is discussed in
+Montenbruck & Phleger book, but Meeus's method is more general and consistent.
+Unit tests use examples from the both sources.
+
+The general problem here is to find the instant of time at which a celestial
+body reaches a predetermined I<altitude>.
+
+To take into account parallax, refraction and apparent radius of the bodies, we
+use average corrections to geometric altitudes:
+
+=over
+
+=item sunrise, sunset : B<-0°50'>
+
+=item moonrise, moonset : B<0°8'>
+
+=item stars and planets : B<-0°34'>
+
+=back
+
+The library also calculates the times of the beginning and end of twilight. The
+types of twilight are:
+
+=over I<astronomical>, when Sun altitude is B<-18°>
+
+=item I<nautical>, when Sun altitude is B<-12°>
+
+=item I<civil>, when Sun altitude is B<-6°>
+
+=back
+
+=head1 EXPORT
+
+=head2 FUNCTIONS
+
+=over
+
+=item * L</rst_event(%args)>
+
+=item * L</twilight(%args)>
+
+=back
+
+=head2 CONSTANTS
+
+=head3 Events
+
+=over
+
+=item * $EVT_RISE — rise
+
+=item * $EVT_SET — set
+
+=item * $EVT_TRANSIT — transit (upper culmination)
+
+=back
+
+=head3 States
+
+=over
+
+=item * $STATE_CIRCUMPOLAR — always above the horizon
+
+=item * $STATE_NEVER_RISES — always below the horizon
+
+=back
+
+=head3 Types of twilight
+
+=over
+
+=item * $TWILIGHT_CIVIL — civil
+
+=item * $TWILIGHT_ASTRO — astronomical
+
+=item * $TWILIGHT_NAUTI — nautical
+
+=back
+
+
+=head1 SUBROUTINES/METHODS
+
+=head2 rst_event(%args)
+
+Returns function for calculating time of given event  L</See Events|Events>.
+
+=head3 Arguments
+
+=over
+
+=item * B<planet>
+
+Celestial body identifier, one of constants defined in L<Astroi::Montenbruck::Ephemeris::Planet>.
+
+=item * B<year> — year, astronomical, zero-based
+
+=item * B<year> — month, 1..12
+
+=item * B<day> — day, 1..31
+
+=item * B<phi> — geographic latitude, degrees, positive northward
+
+=item * B<lambda> — geographic longitude, degrees, positive westward
+
+=back
+
+=head3 Returns
+
+Right Ascension of Meridian, arc-degrees
+
+=cut

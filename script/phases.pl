f8c600c6220ec21289a34870ea6128ef98df4bee
Sergey Krushinsky <krushinsky@gmail.com>
2022-01-31 12:39:22 +0300

    minor fixes, some new examples

--- script/phases.pl
+++ script/phases.pl
@@ -33,14 +33,14 @@ my $theme;
 # Parse options and print usage if there is a syntax error,
 # or if usage was explicitly requested.
 GetOptions(
-    'help|?'     => \$help,
-    'man'        => \$man,
-    'date:s'     => \$date,
-    'theme:s'    => 
-      sub { $theme = Astro::Montenbruck::Utils::Theme->create( $_[1] ) },
-    'no-colors'  => 
-      sub { $theme = Astro::Montenbruck::Utils::Theme->create('colorless') },      
-    'timezone:s' => \$tzone,
+  'help|?'  => \$help,
+  'man'     => \$man,
+  'date:s'  => \$date,
+  'theme:s' =>
+    sub { $theme = Astro::Montenbruck::Utils::Theme->create( $_[1] ) },
+  'no-colors' =>
+    sub { $theme = Astro::Montenbruck::Utils::Theme->create('colorless') },
+  'timezone:s' => \$tzone,
 ) or pod2usage(2);
 
 pod2usage(1)               if $help;
@@ -54,40 +54,45 @@ my $scheme = $theme->scheme;
 my $dt = parse_datetime($date);
 $dt->set_time_zone($tzone) if defined $tzone;
 
-$theme->print_data( 'Date', $dt->strftime('%F'), title_width => 14 );
-$theme->print_data( 'Time Zone', $tzone,         title_width => 14 );
-say();
+$theme->print_data( 'Date',      $dt->strftime('%F'), title_width => 14 );
+$theme->print_data( 'Time Zone', $tzone,              title_width => 14 );
+say;
 
 # find New Moon closest to the date
 my $j = search_event( [ $dt->year, $dt->month, $dt->day ], $NEW_MOON );
 
 # if the event has not happened yet, find the previous one
 if ( $j > $dt->jd ) {
-    my ( $y, $m, $d ) = jd2cal( $j - 28 );
-    $j = search_event( [ $y, $m, floor($d) ], $NEW_MOON );
+  my ( $y, $m, $d ) = jd2cal( $j - 28 );
+  $j = search_event( [ $y, $m, floor($d) ], $NEW_MOON );
 }
 
 my @month    = @MONTH;
-my @quarters = ( { type => pop @month, jd => $j, current => 0 } );
+my @quarters = ( { type => shift @month, jd => $j, current => 0 } );
 push @month, $NEW_MOON;
 
 for my $q (@month) {
-    my ( $y, $m, $d ) = jd2cal $j;
-    $j = search_event( [ $y, $m, floor($d) ], $q );
-    my $prev = $quarters[$#quarters];
-    $prev->{current} = 1 if ( $dt->jd >= $prev->{jd} && $dt->jd < $j );
-    push @quarters, { type => $q, jd => $j, current => 0 };
+  my ( $y, $m, $d ) = jd2cal( $j + 7 );
+  $j = search_event( [ $y, $m, floor($d) ], $q );
+  my $prev = $quarters[$#quarters];
+  if ( $dt->jd >= $prev->{jd} && $dt->jd < $j ) {
+    $prev->{current} = 1;
+  }
+
+  # $prev->{current} = 1 if ( $dt->jd >= $prev->{jd} && $dt->jd < $j );
+  push @quarters, { type => $q, jd => $j, current => 0 };
 }
 
 for my $q (@quarters) {
-    my $dt = DateTime->from_epoch( epoch => jd2unix( $q->{jd} ) )->set_time_zone($tzone);
-    my $mark = $q->{current} ? '*' : ' ';
-    my $data = sprintf( '%s %s', $dt->strftime('%F %T'), $mark );
-   $theme->print_data(
-        $q->{type}, $data,
-        title_width => 14,
-        highlited   => $q->{current}
-    );
+  my $dt =
+    DateTime->from_epoch( epoch => jd2unix( $q->{jd} ) )->set_time_zone($tzone);
+  my $mark = $q->{current} ? '*' : ' ';
+  my $data = sprintf( '%s %s', $dt->strftime('%F %T'), $mark );
+  $theme->print_data(
+    $q->{type}, $data,
+    title_width => 14,
+    highlited   => $q->{current}
+  );
 }
 
 print "\n";
@@ -133,9 +138,9 @@ or I<offset from Greenwich> in format B<+HHMM> / B<-HHMM>, like C<+0300>.
     --timezone=+0300 # UTC + 3h (eastward from Greenwich)
     --timezone="Europe/Moscow"
 
-By default, local timezone by default.
+By default, a local timezone.
 
-Please, note: Windows platform does not recognize some time zone names, C<MSK> for instance.
+Please, note: Windows platform may not recognize some time zone names, like C<MSK>.
 In such cases use I<offset from Greenwich> format, as described above.
 
 
